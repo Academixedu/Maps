beec6fff3cbf2d921a19977fc7985d11d1f9999f
ganeshteki <ganeshteki2001@gmail.com>
2024-08-13 20:58:29 +0530

    maps

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMaps{
 // Preapare a Code that It Should add Person Class Objects
   // Itearate HashMaps using For-Each
+  public static void main(String[] args) {
+    Map<Integer,Person> p=new HashMap<>();
+    p.put(1,new Person("pavs",1,45000,"developer"));
+    p.put(2,new Person("gani",2,50000,"hr"));
+    p.put(3,new Person("rams",3,70000,"manager"));
+    for (Map.Entry<Integer,Person> e : p.entrySet()) {
+      System.out.println(e);
+
+    }
+  }
+
 }
+
--- Person.java
+++ Person.java
@@ -1,3 +1,54 @@
-public class Person{
+
 // Write Your Code Here
-}
+import java.util.Comparator;
+
+    public class Person{
+    // Write Your Code Here
+    private String name;
+    private int id;
+    private int sal;
+    private String desg;
+    public Person(String name, int id, int sal, String desg) {
+        this.name = name;
+        this.id = id;
+        this.sal = sal;
+        this.desg = desg;
+    }
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", id=" + id + ", sal=" + sal + ", desg=" + desg + "]";
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getSal() {
+        return sal;
+    }
+    public void setSal(int sal) {
+        this.sal = sal;
+    }
+    public String getDesg() {
+        return desg;
+    }
+    public void setDesg(String desg) {
+        this.desg = desg;
+    }  
+    static class DesignationComparator implements Comparator<Person> {
+        @Override
+        public int compare(Person p1, Person p2) {
+            return p1.getDesg().compareTo(p2.getDesg());
+        }
+    }
+
+            }
+
+
