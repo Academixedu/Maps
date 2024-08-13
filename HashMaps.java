a1b55c17e2ef1d850cba5817b35f01e6f78e19db
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-13 19:46:46 +0530

    from sai

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMaps{
 // Preapare a Code that It Should add Person Class Objects
   // Itearate HashMaps using For-Each
+  public static void main(String[] args) {
+    Map<Integer,Person> p=new HashMap<>();
+    p.put(4,new Person("sai",21,345,"dev"));
+    p.put(2,new Person("fayaz",21,653,"hr"));
+    p.put(3,new Person("karthik",54,2345,"manager"));
+    for (Map.Entry<Integer,Person> e : p.entrySet()) {
+      System.out.println(e);
+      
+    }
+  }
+
 }
--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,6 +1,25 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class LinkedHashMaps{
 // Preapare a Logic to print LinkedHashMaps in Java 
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
+  public static void main(String[] args) {
+    
+  
+ Map<Integer,Person> h=new HashMap<>();
+h.put(3,new Person("sai",23,3455,"dev"));
+h.put(5,new Person("Fayaz", 21, 1234, "hr"));
+h.put(4,new Person("karthik", 034,4321, "manager"));
+for(Integer i:h.keySet()){
+System.out.println(i);
+}
+for(Person p:h.values()){
+  System.out.println(p);
+}
+
+}
 }
