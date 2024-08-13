f5af14021f67a708629f9972d6f25abe170a51de
Verat-hub <venkey482thalka@gmail.com>
2024-08-13 21:05:02 +0530

    Venkat

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMaps{
 // Preapare a Code that It Should add Person Class Objects
   // Itearate HashMaps using For-Each
+  public static void main(String[] args) {
+    Map<Integer,Person00> p=new HashMap<>();
+      p.put(4,new Person00("RJ.RTN",789,98745,"TL"));
+      p.put(2,new Person00("RTS",456,63210,"AL"));
+      p.put(3,new Person00("PJB",123,12365,"Jl"));
+    for (Map.Entry<Integer,Person00> e : p.entrySet()) {
+      System.out.println(e);
+
+    }
+  }
+
 }
--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,6 +1,25 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class LinkedHashMaps{
 // Preapare a Logic to print LinkedHashMaps in Java 
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
+  public static void main(String[] args) {
+
+
+ Map<Integer,Person00> h=new HashMap<>();
+h.put(3,new Person00("RJ.RTN",789,98745,"TL"));
+h.put(2,new Person00("RTS",456,63210,"AL"));
+h.put(3,new Person00("PJB",123,12365,"Jl"));
+for(Integer i:h.keySet()){
+System.out.println(i);
+}
+for(Person00 p:h.values()){
+  System.out.println(p);
+}
+
+}
 }
