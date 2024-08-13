ef5e7b9bc6204ba1bc2823ecb1303d1082b76224
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-13 20:02:14 +0530

    Update HashMaps.java

--- HashMaps.java
+++ HashMaps.java
@@ -1,10 +1,16 @@
-import java.util.Comparator;
+ import java.util.HashMap;
+import java.util.Map;
 
-public class BasedOnDesg implements Comparator<Person>{
+public class HashMaps{
 
-  @Override
-  public int compare(Person o1, Person o2) {
-    return o1.getDesg().compareTo(o2.getDesg());
-  } 
-
-} 
+  public static void main(String[] args) {
+    Map<Integer,Person> hashmaps = new HashMap<Integer,Person>();
+    hashmaps.put(1,new Person("Bharath",22, "Developer"));
+    hashmaps.put(2,new Person("Shashi",23, "dev"));
+    hashmaps.put(3,new Person("manas",28, "prodev"));
+    hashmaps.put(1,new Person("Acadamix", 77, "HR"));
+    for(Map.Entry<Integer,Person> p : hashmaps.entrySet()){
+      System.out.println(p);
+    }
+  }
+}
