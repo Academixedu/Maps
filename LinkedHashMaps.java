f3f629e8cf113f44dddb3fb1439995578d658310
nayeem6836 <maseednayeem@gmail.com>
2024-08-13 20:51:13 +0530

    Update LinkedHashMaps.java

--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,19 +1,22 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-
-public class HashMaps{
-// Preapare a Code that It Should add Person Class Objects
-  // Itearate HashMaps using For-Each
-  public static void main(String[] args) {
- Map<Integer,Person> p = new HashMap<>();
+public class LinkedHashMaps{
+// Preapare a Logic to print LinkedHashMaps in Java 
+  // Itearatre it using EntrySet
+  // Print Only Values
+  // Print Only Keys
+   public static void main(String[] args) {
+ Map<Integer,Person> p = new LinkedHashMap<>();
  p.put(1,new Person("fayaz", 21, "dev", 25000));
  p.put(3,new Person("karthik", 21, "hr", 26000));
  p.put(2,new Person("sai", 21, "dev", 27000));
- for(Map.Entry<Integer, Person> per:p.entrySet()){
-  System.out.println(per);;
+  for(Integer i:p.keySet()){
+    System.out.println(i);
  }
+ for(Person a:p.values()){
+  System.out.println(a);
 
-
+ }
 }
 }
