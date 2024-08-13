f2d96bb682f9fa3e753ec0acf057a70a645710d6
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-13 16:45:25 +0530

    Create TreeMaps.java

---
+++ TreeMaps.java
@@ -0,0 +1,6 @@
+public class TreeMaps{
+// Preapare a Logic to add Person Class Objects using Tree Maps
+  // Print Only Keys By sorting order of desg
+  // Print Only Values By sortin order of desg
+  // print both keys and values
+}
