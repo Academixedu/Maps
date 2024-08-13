2fbbc00269a96f29a170acba42fedf175c7f13d8
psvsai <pasupuletisvsm245@gmail.com>
2024-08-13 21:19:32 +0530

    PSVSAI

--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,6 +1,24 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
 public class LinkedHashMaps{
 // Preapare a Logic to print LinkedHashMaps in Java 
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
+  public static void main(String[] args) {
+   LinkedHashMap<Integer,Person> p = new LinkedHashMap<>();
+   p.put(1, "Mahesh");
+   p.put(1, "PSVSAI");
+   p.put(2, "Venkata");
+   for(Integer i:p.keySet()){
+    System.out.println(i);
+   }
+   for(Person a: p.values()){
+    System.out.println(a);
+   }
+   for (Map.Entry<Integer, Person> entry : p.entrySet()) {
+    System.out.println(entry);
 }
+}
+  }
+
--- TreeMaps.java
+++ TreeMaps.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TreeMaps{
 // Preapare a Logic to add Person Class Objects using Tree Maps
   // Print Only Keys By sorting order of desg
   // Print Only Values By sortin order of desg
   // print both keys and values
+  public static void main(String[] args) {
+    Map<Integer,Person> p = new TreeMap<>();
+    p.put(1, "PSVSAI");
+    p.put(1, "PSVSAI");
+    p.put(2, "sai");
+    p.put(3, "hi");
+    for (Integer key : p.keySet()) {
+      System.out.println(key);
+  }
+  for (Person person : p.values()) {
+    System.out.println(person);
+}
+   
+  }
 }
