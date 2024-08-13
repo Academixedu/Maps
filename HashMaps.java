564e190c71c21a2dcb3914449d516a3034d6a54a
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-13 20:48:32 +0530

    sarvesh

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
 public class HashMaps{
 // Preapare a Code that It Should add Person Class Objects
   // Itearate HashMaps using For-Each
+
+  public static void main(String[] args) {
+    Map<Integer,String>p=new HashMap();
+    p.put(1, "sarvesh");
+    p.put(3, "ratan");
+    p.put(2, "abhi");
+    p.put(4, "choko");
+    p.put(1, "saru");
+
+    for ( Entry<Integer,String>string:p.entrySet()) {
+      System.out.println(string);
+  }
+
+
+
+
+      
+    
+  }
+
+
+
+
 }
--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,6 +1,42 @@
+import java.util.Map;
+import java.security.KeyStore.Entry;
+import java.util.LinkedHashMap;
+// import java.util.Map.Entry;
+
+
 public class LinkedHashMaps{
 // Preapare a Logic to print LinkedHashMaps in Java 
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
+public static void main(String[] args) {
+  
+  Map<Integer,String>s=new LinkedHashMap<Integer,String>();
+
+  s.put(1,"Sarvesh");
+  s.put(4,"Abhi");
+  s.put(1,"Saru");
+  s.put(3,"ratan");
+  s.put(2,"choco");
+
+for(Map.Entry<Integer,String> entry:s.entrySet()){
+
+Integer key=entry.getKey();
+String value=entry.getValue();
+System.out.println("kays:"+key+" value:"+value);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
 }
