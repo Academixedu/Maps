ce83d4dfa27b4cba924e0586b756b0995868b47a
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-13 19:33:57 +0530

    From Karthik

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMaps{
-// Preapare a Code that It Should add Person Class Objects
-  // Itearate HashMaps using For-Each
+
+  public static void main(String[] args) {
+    Map<Integer,Person> hashmaps = new HashMap<Integer,Person>();
+    hashmaps.put(1,new Person("Karthik",21, "Dev"));
+    hashmaps.put(2,new Person("Sai",20, "DEV"));
+    hashmaps.put(3,new Person("Fayaz",21, "TESTER"));
+    hashmaps.put(1,new Person("Karthik", 22, "HR"));
+    for(Map.Entry<Integer,Person> p : hashmaps.entrySet()){
+      System.out.println(p);
+    }
+  }
 }
