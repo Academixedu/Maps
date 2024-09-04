df3907fcec631e5d117fe02a6f9128f777083bb0
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-09-04 16:51:30 +0530

    Update LinkedHashMaps.java

--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,6 +1,24 @@
-public class LinkedHashMaps{
-// Preapare a Logic to print LinkedHashMaps in Java 
+import java.util.*;
+
+public class HashMaps {
+  // Preapare a Logic to print LinkedHashMaps in Java
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
+  public static void main(String[] args) {
+    LinkedHashMap<Integer, String> ls = new LinkedHashMap<Integer, String>();
+    ls.put(12, "hello");
+    ls.put(98, "oops");
+    ls.put(76, "yup");
+    ls.put(86, "yeah");
+    for (Map.Entry<Integer, String> entry : ls.entrySet()) {
+      System.out.println("the values " + entry.getValue());
+    }
+    for (Map.Entry<Integer, String> entry1 : ls.entrySet()) {
+      System.out.println("the key" + entry1.getKey());
+    }
+
+
+  }
+
 }
