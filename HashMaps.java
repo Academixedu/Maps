dd308cf6058a8bbd8f03e75aee93d296ed8d007d
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-09-04 19:59:29 +0530

    shahid

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class HashMaps{
 // Preapare a Code that It Should add Person Class Objects
-  // Itearate HashMaps using For-Each
+ public static void main(String[] args) {
+  
+
+   HashMap<Person,String>v=new HashMap<>();
+   v.put(new Person(1,"rohit","head"),"Hi");
+   v.put(new Person(2,"rohit","Hr"),"hh");
+   v.put(new Person(1,"hello","Hr"),"oo");
+   for(Map.Entry<Person,String>r:v.entrySet()){
+       System.out.println(r);
+
+}
+}
 }
--- TreeMaps.java
+++ TreeMaps.java
@@ -1,6 +1,37 @@
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Comparator;
 public class TreeMaps{
 // Preapare a Logic to add Person Class Objects using Tree Maps
   // Print Only Keys By sorting order of desg
   // Print Only Values By sortin order of desg
   // print both keys and values
+  public static void main(String[] args) {
+  Comparator d=new desg();
+  Scanner y=new Scanner(System.in);
+  System.out.println("Enter any value");
+  String m=y.next();
+  
+  TreeMap<Person,String> n=new TreeMap<>(d);
+  n.put(new Person(1,"rello","head"),"1");
+  n.put(new Person(2,"hohit","Hr"),"2");
+  n.put(new Person(3,"arbaz","Hr"),"1");
+ if(m.contains("KEYS")){
+   for(Map.Entry<Person,String> v:n.entrySet()){
+  System.out.println(v.getKey());
+   }
+   else if(m.contains("values")){
+     for(Map.Entry<Person,String> v:n.entrySet()){
+       System.out.println(v.getValue());
+        }
+
+}
+ }
+  // // for (Map.Entry<String, Person> v:m.entrySet()) {
+  //   //     System.out.println(v.getKey()); 
+  //   // }
+  //   System.out.println("Keys sorted by desg:");
+  // System.out.println("values sorted by desg:");
+}
 }
