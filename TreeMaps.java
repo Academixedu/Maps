46766d9cec7f617c164310922048544f2d20719e
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-09-04 20:05:29 +0530

    save

--- TreeMaps.java
+++ TreeMaps.java
@@ -1,6 +1,49 @@
-public class TreeMaps{
-// Preapare a Logic to add Person Class Objects using Tree Maps
-  // Print Only Keys By sorting order of desg
-  // Print Only Values By sortin order of desg
-  // print both keys and values
+import java.util.Map;
+import java.util.TreeMap;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+}
+
+public class TreeMaps {
+    public static void main(String[] args) {
+        TreeMap<String, Person> treeMap = new TreeMap<>();
+
+        treeMap.put("3", new Person("Alice", 30));
+        treeMap.put("1", new Person("Bob", 25));
+        treeMap.put("2", new Person("Charlie", 35));
+
+        System.out.println("Keys in TreeMap:");
+        for (String key : treeMap.keySet()) {
+            System.out.println(key);
+        }
+
+        System.out.println("\nValues in TreeMap:");
+        for (Person person : treeMap.values()) {
+            System.out.println(person);
+        }
+        System.out.println("\nKeys and Values in TreeMap:");
+        for (Map.Entry<String, Person> entry : treeMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
 }
