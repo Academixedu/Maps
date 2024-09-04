0bd22b4e66add200f13aa5111c2c1898c29828c6
Rohit08012004 <rohitsthakre@gmail.com>
2024-09-04 19:32:02 +0530

    done

--- TreeMaps.java
+++ TreeMaps.java
@@ -8,19 +8,31 @@ public class TreeMaps{
 
 
 
-    TreeMap<Person,String>k=new TreeMap<>();
+    TreeMap<String,Person>k=new TreeMap<>();
 
 
 
-    k.put(new Person(1,"orhit",4000), "rohit");
-    k.put(new Person(2,"mohit",3000), "mohit");
-    k.put(new Person(3,"nanda", 90000), "nanda");
+    k.put("rohit",new Person(1,"orhit",4000));
+    k.put( "mohit",new Person(2,"mohit",3000));
+    k.put( "nanda",new Person(3,"nanda", 90000));
   
 
     
-        for (Entry<Person, String> key :k.entrySet()) {
-            System.out.println(key);
-        }
+    for (String key :k.keySet()) {
+      System.out.println(key);
+  }
+
+  // Print only values sorted by designation (in the order of keys)
+  System.out.println("\nValues (Persons) in sorted order by designation:");
+  for (Person person : k.values()) {
+      System.out.println(person);
+  }
+
+  // Print both keys and values
+  System.out.println("\nBoth keys and values:");
+  for (Entry<String, Person> entry : k.entrySet()) {
+      System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+  }
 
         // System.out.println("\nValues (Persons) in sorted order by designation:");
         // for (Person person : Person.values()) {
@@ -29,7 +41,7 @@ public class TreeMaps{
 
         
       
-        for (Entry<Person, String> entry : k.entrySet()) {
+        for (Entry<String, Person> entry : k.entrySet()) {
             System.out.println(entry);
         }
 
