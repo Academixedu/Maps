9ca1c0f7e80136839f01f4c193bc3bd2715f5182
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-09-04 18:08:02 +0530

    Update TreeMaps.java

--- TreeMaps.java
+++ TreeMaps.java
@@ -1,6 +1,32 @@
-public class TreeMaps{
-// Preapare a Logic to add Person Class Objects using Tree Maps
+package okay;
+
+import java.util.*;
+
+public class TreeMaps {
+  // Preapare a Logic to add Person Class Objects using Tree Maps
   // Print Only Keys By sorting order of desg
   // Print Only Values By sortin order of desg
   // print both keys and values
+  public static void main(String[] args) {
+    Comparator p12 = new basedondesg();
+    TreeMap<Person, String> map = new TreeMap<Person, String>(p12);
+    Person p = new Person("hello", 21, 34, "Developer");
+    map.put(p, "hello");
+    p = new Person("world", 18, 27, "Manager");
+    map.put(p, "world");
+    p = new Person("global", 23, 27, "worker");
+    map.put(p, "global");
+
+    for (Map.Entry<Person, String> entry : map.entrySet()) {
+      System.out.println("Key: " + entry.getKey());
+    }
+    Iterator<Map.Entry<Person, String>> it = map.entrySet().iterator();
+    while (it.hasNext()) {
+      Map.Entry<Person, String> pair = it.next();
+      System.out.println("Value: " + pair.getValue());
+    }
+    for (Map.Entry<Person, String> entry : map.entrySet()) {
+      System.out.println("Keys: " + entry.getKey() + ", Values: " + entry.getValue());
+    }
+  }
 }
