c89dd04d25017c89cde2b97c9f39782aa54d8d2f
Arbaz-01 <arbazshaibu@gmail.com>
2024-09-04 19:44:00 +0530

    AZZY

--- TreeMaps.java
+++ TreeMaps.java
@@ -36,9 +36,6 @@ System.out.println("PRINTING KEYS ONLY SORTING ORDER OF DESG");
 
   
 
-// Preapare a Logic to add Person Class Objects using Tree Maps
-  // Print Only Keys By sorting order of desg
-  // Print Only Values By sortin order of desg
-  // print both keys and values
+
 }
 }
