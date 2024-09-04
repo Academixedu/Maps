bb1f411cbc14f3fa4b8d1413fe7799ab64d204d7
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-09-04 19:02:17 +0530

    linkedhashmap

--- LinkedHashMaps.java
+++ LinkedHashMaps.java
@@ -1,10 +1,20 @@
+import java.util.*;
 public class LinkedHashMaps{
 // Preapare a Logic to print LinkedHashMaps in Java 
   // Itearatre it using EntrySet
   // Print Only Values
   // Print Only Keys
   public static void main(String[] args) {
-     LinkedHashMaps<>
+     LinkedHashMap<Integer,String>lh=new LinkedHashMap<>();
+     lh.put(3, "vijay");
+     lh.put(5, "marsh");
+
+     Iterator<Map.Entry<Integer,String>> il=lh.entrySet().iterator();
+     while (il.hasNext()) {
+      Map.Entry<Integer,String> in=il.next();
+      System.out.println(in.getKey()+ " " +in.getValue());
+      
+     }
 
 
   }
